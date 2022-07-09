577e264cba8ddba486f20153afb900e119229a15
Aarush Shintre <65198209+AarushShintre@users.noreply.github.com>
2022-07-09 19:57:32 +0530

    heroku deployment changes

--- website/__init__.py
+++ website/__init__.py
@@ -7,28 +7,30 @@ DB_NAME = "database.db"
 
 db = SQLAlchemy()
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'some-secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://helmascaqbnjcy:d58ede02612842cf7f5f92ea352840187b8fe1217c2fffdae9b6acd7305a552b@ec2-34-233-115-14.compute-1.amazonaws.com:5432/dfumocahrfo52p'
-    db.init_app(app)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'some-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://helmascaqbnjcy:d58ede02612842cf7f5f92ea352840187b8fe1217c2fffdae9b6acd7305a552b@ec2-34-233-115-14.compute-1.amazonaws.com:5432/dfumocahrfo52p'
+db.init_app(app)
 
-    from .views import views
-    from .auth import auth
+from .views import views
+from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+app.register_blueprint(views, url_prefix='/')
+app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User
+from .models import User
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
-    return app
+
+if __name__ == "__main__":
+        app.run()
+
 
 
